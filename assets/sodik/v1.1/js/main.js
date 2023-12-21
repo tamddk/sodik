e265d2b1da1cfb07f352370f8ced7ac57f9b137e
tamddk <siapaa.akuhh@gmail.com>
2023-12-21 23:21:21 +0700

    Initial commit

--- assets/sodik/v1.1/js/main.js
+++ assets/sodik/v1.1/js/main.js
@@ -266,7 +266,19 @@ const apiUrl = `https://newsapi.org/v2/top-headlines?country=id&apiKey=${apiKey}
 
 async function fetchNews() {
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(apiUrl, {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: "", // body data type must match "Content-Type" header
+    });
     const data = await response.json();
     displayNews(data.articles)
   } catch (error) {
